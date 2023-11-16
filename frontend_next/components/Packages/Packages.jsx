258d9a5e735e70b0fdeda89b7418e59ee9af99ee
Tasim <ttasimer6@gmail.com>
2023-11-16 12:25:50 +0200

    тасимцхо

--- frontend_next/components/Packages/Packages.jsx
+++ frontend_next/components/Packages/Packages.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Packages = () => {
   return (
-    <div>Packages</div>
+    <section id="packages">
+      <div className="container">
+        <div className="title">
+          <h2>Персонализирани тренировки специално изработени за теб.</h2>
+        </div>
+        <div className="package-container">
+          <div className="package package-first">
+            <div className="package-title">
+              <h3>Begginer</h3>
+              <span>Започни първата крачка към целите си</span>
+            </div>
+            <ul>
+              <span>Съдържание </span>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
