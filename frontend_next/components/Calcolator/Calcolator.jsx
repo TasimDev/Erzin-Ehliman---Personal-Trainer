4311c0f461ac3659e32634f302e150f43302c05b
Tasim <118922118+TasimDev@users.noreply.github.com>
2023-11-18 20:21:00 +0200

    clients done

--- frontend_next/components/Calcolator/Calcolator.jsx
+++ frontend_next/components/Calcolator/Calcolator.jsx
@@ -1,9 +1,9 @@
 
 'use client'
-import { useState} from 'react'; 
+import { useState } from 'react';
 
 const Calcolator = () => {
-  
+
   const [weight, setWeight] = useState('');
   const [height, setHeight] = useState('');
   const [age, setAge] = useState('');
@@ -58,80 +58,80 @@ const Calcolator = () => {
 
   return (
     <section id="calcolator">
-       <div className="title">
-          <h2>Калориен Калколатор</h2>
-        
-        </div>
+      <div className="title">
+        <h2>Калориен Калкулатор</h2>
+
+      </div>
       <div className="container">
-       
+
         <div className="calcolator-side">
-            <div className='form'>
-              <div className='form-section'>
+          <div className='form'>
+            <div className='form-section'>
               <div className="form-input">
                 <label htmlFor="height" className='label-title'>Височина</label>
-                <input type="text" name="height" id="height" placeholder='см' required value={height} onChange={(e) => setHeight(e.target.value)}/>
+                <input type="text" name="height" id="height" placeholder='см' required value={height} onChange={(e) => setHeight(e.target.value)} />
               </div>
               <div className="form-input">
                 <label htmlFor="wight" className='label-title'>Тегло</label>
-                <input type="text" name="wight" id="weight" placeholder='кг' required value={weight} onChange={(e) => setWeight(e.target.value)}/>
+                <input type="text" name="wight" id="weight" placeholder='кг' required value={weight} onChange={(e) => setWeight(e.target.value)} />
               </div>
-              </div>
-              <div className='form-section'>
+            </div>
+            <div className='form-section'>
               <div className="form-input">
                 <label htmlFor="year" className='label-title'>Години</label>
-                <input type="number" name="year" id="year" placeholder='2-80год.' required value={age} onChange={(e) => setAge(e.target.value)}/>
+                <input type="number" name="year" id="year" placeholder='2-80год.' required value={age} onChange={(e) => setAge(e.target.value)} />
               </div>
               <div className="form-input">
-              <label className='label-title'>Пол</label>
-              <div className="radio-container">
-                <div className="radio-div">
-              <label htmlFor="male">Мъж</label>
-              <input type="radio" name="sex" id="male" value='male' required checked={gender === 'male'}
-            onChange={() => setGender('male')} />
+                <label className='label-title'>Пол</label>
+                <div className="radio-container">
+                  <div className="radio-div">
+                    <label htmlFor="male">Мъж</label>
+                    <input type="radio" name="sex" id="male" value='male' required checked={gender === 'male'}
+                      onChange={() => setGender('male')} />
+                  </div>
+                  <div className="radio-div">
+                    <label htmlFor="female">Жена</label>
+                    <input type="radio" name="sex" id="female" value='female' required checked={gender === 'female'}
+                      onChange={() => setGender('female')} />
+                  </div>
+                </div>
               </div>
-              <div className="radio-div">
-              <label htmlFor="female">Жена</label>
-              <input type="radio" name="sex" id="female" value='female' required checked={gender === 'female'}
-            onChange={() => setGender('female')}/>
-              </div>
-              </div>
-              </div>
-              </div>
-              <div className="form-section-option">
+            </div>
+            <div className="form-section-option">
               <div className="form-input">
-              <label>Тренировачно ниво: </label>
-                   <select value={activityLevel} onChange={(e) => setActivityLevel(e.target.value)} >
-                       <option value="lifebasis">Базиран на начина на живот</option>
-                        <option value="active">Активен (2-3 седмично)</option>
-                       <option value="normalactive">Нормална активност (4 - 5)</option>
-                      <option value="verryactive">Много Активен (6 дена седмично)</option>
-                  </select>
+                <label>Тренировачно ниво: </label>
+                <select value={activityLevel} onChange={(e) => setActivityLevel(e.target.value)} >
+                  <option value="lifebasis">Базиран на начина на живот</option>
+                  <option value="active">Активен (2-3 седмично)</option>
+                  <option value="normalactive">Нормална активност (4 - 5)</option>
+                  <option value="verryactive">Много Активен (6 дена седмично)</option>
+                </select>
               </div>
-              </div>
-              <input type="submit" value="Изчисли" onClick={calculateCalories} />
-              </div>
+            </div>
+            <input type="submit" value="Изчисли" onClick={calculateCalories} />
+          </div>
         </div>
         <div className="line-div"></div>
         <div className="result-side">
           <div className="result-title"><span>Резултат</span></div>
           {calories !== null && !isNaN(calories) && (
-        <div className='result-container'>
-         <div className="result-item">
-                    <span className='result-item-title'>Поддържане на тегло: </span>
-                    <div className="result">{calories}<br /><span>калории/ден</span></div>
-                  </div>
-                  <div className="result-item">
-                    <span className='result-item-title'>Покачване на тегло: </span>
-                    <div className="result">{maxCalories} <br /><span>калории/ден</span></div>
-                  </div>
-                  <div className="result-item">
-                    <span className='result-item-title'>Плавно отслабване: </span>
-                    <div className="result"> {minCalories}<br /><span>калории/ден</span></div>
-                  </div>
-         
-        </div>
-      )}
-            
+            <div className='result-container'>
+              <div className="result-item">
+                <span className='result-item-title'>Поддържане на тегло: </span>
+                <div className="result">{calories}<br /><span>калории/ден</span></div>
+              </div>
+              <div className="result-item">
+                <span className='result-item-title'>Покачване на тегло: </span>
+                <div className="result">{maxCalories} <br /><span>калории/ден</span></div>
+              </div>
+              <div className="result-item">
+                <span className='result-item-title'>Плавно отслабване: </span>
+                <div className="result"> {minCalories}<br /><span>калории/ден</span></div>
+              </div>
+
+            </div>
+          )}
+
         </div>
       </div>
     </section>
--- frontend_next/components/Clients/Clients.jsx
+++ frontend_next/components/Clients/Clients.jsx
@@ -3,9 +3,51 @@
 const Clients = () => {
   return (
     <section id="clients">
-      <ul className="clients-items">
-       
-      </ul>
+      <div className="title">
+        <h2>Трансформации</h2>
+      </div>
+      <div className="clients-items">
+        <div className="client-item">
+          <img src="/assets/trandormation.jpg" alt="tranfprmation" />
+          <div className="info">
+            <div className="name">Josh Quote</div>
+            <div className="tesominal">
+              "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore, at corrupti asperiores rem minima sunt! "
+            </div>
+            <span className="transformation">2 месеца</span>
+          </div>
+        </div>
+        <div className="client-item">
+          <img src="/assets/trandormation.jpg" alt="tranfprmation" />
+          <div className="info">
+            <div className="name">Josh Quote</div>
+            <div className="tesominal">
+              "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore, at corrupti asperiores rem minima sunt! "
+            </div>
+            <span className="transformation">2 месеца</span>
+          </div>
+        </div>
+        <div className="client-item">
+          <img src="/assets/trandormation.jpg" alt="tranfprmation" />
+          <div className="info">
+            <div className="name">Josh Quote</div>
+            <div className="tesominal">
+              " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore, at corrupti asperiores rem minima sunt! "
+            </div>
+            <span className="transformation">2 месеца</span>
+          </div>
+        </div>
+        <div className="client-item">
+          <img src="/assets/trandormation.jpg" alt="tranfprmation" />
+          <div className="info">
+            <div className="name">Josh Quote</div>
+            <div className="tesominal">
+              " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore, at corrupti asperiores rem minima sunt! "
+            </div>
+            <span className="transformation">2 месеца</span>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
